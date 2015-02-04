918a8012017d781fa22f9768e0c9b4781735962c
Anthony Rappa <rappa819@gmail.com>
2015-02-04 11:40:48 -0500

    Simplified config docs

--- src/config/vault.php
+++ src/config/vault.php
@@ -18,65 +18,34 @@ return array(
 	],
 
 	/*
-	|--------------------------------------------------------------------------
-	| Vault Role Model
-	|--------------------------------------------------------------------------
-	|
-	| This is the Role model used by Vault to create correct relations.  Update
-	| the role if it is in a different namespace.
-	|
+	 * Role model used by Vault to create correct relations. Update the role if it is in a different namespace.
 	*/
 	'role' => 'Rappasoft\Vault\VaultRole',
 
 	/*
-	|--------------------------------------------------------------------------
-	| Vault Roles Table
-	|--------------------------------------------------------------------------
-	|
-	| This is the Roles table used by Vault to save roles to the database.
-	|
-	*/
+	 * Roles table used by Vault to save roles to the database.
+	 */
 	'roles_table' => 'roles',
 
 	/*
-	|--------------------------------------------------------------------------
-	| Vault Permission Model
-	|--------------------------------------------------------------------------
-	|
-	| This is the Permission model used by Vault to create correct relations.  Update
-	| the permission if it is in a different namespace.
-	|
-	*/
+	 * Permission model used by Vault to create correct relations.
+	 * Update the permission if it is in a different namespace.
+	 */
 	'permission' => 'Rappasoft\Vault\VaultPermission',
 
 	/*
-	|--------------------------------------------------------------------------
-	| Vault Permissions Table
-	|--------------------------------------------------------------------------
-	|
-	| This is the Permissions table used by Vault to save permissions to the database.
-	|
-	*/
+	 * Permissions table used by Vault to save permissions to the database.
+	 */
 	'permissions_table' => 'permissions',
 
 	/*
-	|--------------------------------------------------------------------------
-	| Vault permission_role Table
-	|--------------------------------------------------------------------------
-	|
-	| This is the permission_role table used by Vault to save relationship between permissions and roles to the database.
-	|
-	*/
+	 * permission_role table used by Vault to save relationship between permissions and roles to the database.
+	 */
 	'permission_role_table' => 'permission_role',
 
 	/*
-	|--------------------------------------------------------------------------
-	| Vault assigned_roles Table
-	|--------------------------------------------------------------------------
-	|
-	| This is the assigned_roles table used by Vault to save assigned roles to the database.
-	|
-	*/
+	 * assigned_roles table used by Vault to save assigned roles to the database.
+	 */
 	'assigned_roles_table' => 'assigned_roles',
 
 	/*
